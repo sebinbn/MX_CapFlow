cb8f41b413b2891eac1eb357ac8db79d443388ee
unknown <sebinbn@gmail.com>
2024-09-23 22:54:54 -0500

    Reviewed and added comments, renamed variables so as to be later used in SVAR analysis

--- 2_ADFtest.R
+++ 2_ADFtest.R
@@ -1,31 +1,33 @@
-# 26 aug - file created by copying code, but not reviewed
+# This file runs ADF test on levels and first differences and the results are
+# stored in ADF_tab. The ADF test is run on weekly data using Mex_w. The first
+# differenced Mex_w is later used in 2_SVAR
 
 library(urca)         #for ur.df
 
 # Creating date indices for subsetting ------------------------------------
-Index = cbind(Mex_w$Date <= as.Date("2011-12-31") & Mex_w$Date >= as.Date("2010-01-01"),
+subsamp_Index = cbind(Mex_w$Date <= as.Date("2011-12-31") & Mex_w$Date >= as.Date("2010-01-01"),
           Mex_w$Date <= as.Date("2015-12-31") & Mex_w$Date >= as.Date("2014-01-01"),
           Mex_w$Date <= as.Date("2013-12-31") & Mex_w$Date >= as.Date("2012-01-01"))
 
-Vars_ADF = c("TIIE","MPTBA","GMXN10Y","MXN_USD")                                  #List of variables ADF test is to be run on
+Vars_SVAR = c("TIIE","MPTBA","GMXN10Y","MXN_USD")                                  #List of variables ADF test and later SVAR is to be run on
 
 
 # Creating table to store results -----------------------------------------
 
-nvar = length(Vars_ADF)                                                           #nvar and nsubsamp are used later and so storing as a variable is useful
-nsubsamp = ncol(Index)
+nvar = length(Vars_SVAR)                                                           #nvar and nsubsamp are used later and so storing as a variable is useful
+nsubsamp = ncol(subsamp_Index)
 ADF_tab = matrix(NaN, nvar*2, nsubsamp*2)
 colnames(ADF_tab) = paste(c(rep("Jan2010_Dec2011",2),rep("Jan2014_Dec2015",2),
                       rep("Jan2012_Dec 2013",2)), rep(c("Lvl","1Diff"),3), sep = "_" )
 x = NaN
 for (Var in 1:nvar){
-  x = c(x,paste(rep(Vars_ADF[Var],2), c("ADF","pValue"), sep = "_") )
+  x = c(x,paste(rep(Vars_SVAR[Var],2), c("ADF","pValue"), sep = "_") )
 }
 rownames(ADF_tab) = x[-1]
 
 # Creating first differences ----------------------------------------------
 
-Mex_w_d = lapply(Mex_w[,Vars_ADF], diff)                                          #lapply has to be used because diff() does not work on dartaframes
+Mex_w_d = lapply(Mex_w[,Vars_SVAR], diff)                                          #lapply has to be used because diff() does not work on dartaframes
 Mex_w_d = data.frame(Date = Mex_w$Date[-1], Mex_w_d)
 
 # Running ADF test and storing results-------------------------------------
@@ -34,9 +36,9 @@ Mex_w_d = data.frame(Date = Mex_w$Date[-1], Mex_w_d)
 
 for (subsamp in  1:nsubsamp){                                                            #Comparing speeds and reading online, for loop are not any slower (and sometimes faster) than lapply
   for (Var in 1:nvar){
-    adf_l = summary(ur.df(Mex_w[Index[,subsamp],Vars_ADF[Var]], type = "none", 
+    adf_l = summary(ur.df(Mex_w[subsamp_Index[,subsamp],Vars_SVAR[Var]], type = "none", 
                           selectlags = "AIC"))
-    adf_d = summary(ur.df(Mex_w_d[Index[,subsamp],Vars_ADF[Var]], type = "none",
+    adf_d = summary(ur.df(Mex_w_d[subsamp_Index[,subsamp],Vars_SVAR[Var]], type = "none",
                           selectlags = "AIC"))
     ADF_tab[c(2*Var - 1,2* Var),(2*subsamp - 1)] = 
       adf_l@testreg$coefficients[1,c("t value", "Pr(>|t|)")]
@@ -54,7 +56,7 @@ for (subsamp in  1:nsubsamp){                                                   
 
 # Removing excess variables -----------------------------------------------
 
- rm(x,nvar,nsubsamp, adf_l, adf_d, Index, Vars_ADF, subsamp, Var)
+ rm(x,nvar,nsubsamp, adf_l, adf_d, subsamp, Var)
 
 
 
