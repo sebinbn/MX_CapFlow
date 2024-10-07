312dc0305e16a6e4e6c4360de417c24d22f2ec56
Sebin Nidhiri <sebinbn@gmail.com>
2024-10-07 12:12:03 -0500

    Create SVAR table with more columns, now including 6mo,1y,2y,5y and 30y yields

--- 1_TabCreate_SVAR.R
+++ 1_TabCreate_SVAR.R
@@ -1,5 +1,5 @@
 # This file merges the various data to create a single table to be used in SVAR.
-# It creaetes the daily frequency table and then the weekly frequency table using
+# It creates the daily frequency table and then the weekly frequency table using
 # end of week daily data.
 
 
@@ -9,14 +9,15 @@ MergedTab  = merge(BBYield, Mex_FO)
 MergedTab = merge(MergedTab, MXN)
 MergedTab = merge(MergedTab, TIIE, all= T)
 
-VarsExport = c( "Date","MPTBA", "GMXN10Y", "MXN_USD","TIIE", "F_Own_p","Tgt_rate")
+VarsExport = c( "Date","MPTBA", "MPTBF", "GMXN01Y","GMXN02Y", "GMXN05Y", 
+                "GMXN10Y", "GMXN30Y","MXN_USD","TIIE", "F_Own_p","Tgt_rate")
 
 # Creating Daily Data  ----------------------------------------------------
 
 Mex_d = MergedTab[MergedTab$Date <= as.Date("2023-12-31") & 
                     MergedTab$Date >= as.Date("2006-01-01"), VarsExport]            #Creating subset of  daily data for SVAR required variables from 2006
                  
-write.csv(Mex_d, "Mexicopaper_Data/Mex_Daily.csv", row.names = F)                                 #Exporting daily data
+write.csv(Mex_d, "Mexicopaper_Data/Mex_Daily_MH.csv", row.names = F)                                 #Exporting daily data
 
 # Creating Weekly Data  ---------------------------------------------------
 
@@ -30,11 +31,12 @@ write.csv(Mex_d, "Mexicopaper_Data/Mex_Daily.csv", row.names = F)               
 
 Sun  = seq(as.Date("2006-01-01"), as.Date("2023-12-31"), by = "7 days")            #creating vector of Sundays. 2006-01-01 was Sunday.
 
-Mex_w = data.frame(Date = Sun, Values = matrix(NA, length(Sun),6))                #Initiating a dataframe
+Mex_w = data.frame(Date = Sun, Values = matrix(NA, length(Sun),
+                                               length(VarsExport) -1) )                #Initiating a dataframe
 colnames(Mex_w) = VarsExport
 
 colMeans(is.na(Mex_d))                                                            #checking which variables have missing values - only MTBA and GMXN10y have missing values
-length(Mex_d$Date[is.na(Mex_d$Tgt_rate)])                                         #code to ebe edited to check how many and which dates each variable is missing
+length(Mex_d$Date[is.na(Mex_d$Tgt_rate)])                                         #code to be edited to check how many and which dates each variable is missing
 
 for(i in 1:length(Sun)){                                                          
   Week_Data = MergedTab[MergedTab$Date <= Sun[i] & MergedTab$Date > (Sun[i]-7),
@@ -45,6 +47,7 @@ for(i in 1:length(Sun)){
     Mex_w[i, V] = if(nrow(a) != 0) tail(a,1) else NA
   }
 }
+colMeans(is.na(Mex_w))
 
 write.csv(Mex_w, "Mexicopaper_Data/Mex_weekly.csv", row.names = F)                                 #Exporting weekly data
 
