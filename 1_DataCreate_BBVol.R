fe39df741dcf7e103c5fd3317fe76a2fc1b1e0ab
Sebin Nidhiri <sebinbn@gmail.com>
2024-10-09 11:39:31 -0500

    Weekly data created. Volatility renamed as Liquidity

--- 1_DataCreate_BBVol.R
+++ 1_DataCreate_BBVol.R
@@ -1,6 +1,6 @@
 #This R file does the following
 
-# 1. Import bid, ask, mid data downloaded from Bloomberg and converts to date 
+# 1. Import mid, ask, bid data downloaded from Bloomberg and converts to date 
 # and numeric.
 # 2. It finds the date from when each variable's data is available
 
@@ -13,42 +13,44 @@
 # are renamed to indicate whether it is mid, ask or bid. Similar to BBYield, data
 # has all dates and for days when market was closed, the value is NA.It is ordered
 # descending by dates and is changed to ascending.
-BBVol = read_xlsx("BB_Data/MXYC_1mo30y_PBAM.xlsx", range = "A2:BC8891")
-colnames(BBVol)[1] = 'Date'
-colnames(BBVol)[-1] = c(
-  paste('M', substr(colnames(BBVol)[2:14], 5,8), sep = "_"),
-  paste('M', substr(colnames(BBVol)[15:19], 3,5), sep = "_"),
-  paste('A', substr(colnames(BBVol)[20:32], 5,8), sep = "_"),
-  paste('A', substr(colnames(BBVol)[33:37], 3,5), sep = "_"),
-  paste('B', substr(colnames(BBVol)[38:50], 5,8), sep = "_"),
-  paste('B', substr(colnames(BBVol)[51:55], 3,5), sep = "_") )
+BBLiq = read_xlsx("BB_Data/MXYC_1mo30y_PBAM.xlsx", range = "A2:BC8891")
+colnames(BBLiq)[1] = 'Date'
+colnames(BBLiq)[-1] = c(
+  paste('M', substr(colnames(BBLiq)[2:14], 5,7), sep = "_"),
+  paste('M', substr(colnames(BBLiq)[15:19], 3,5), sep = "_"),
+  paste('A', substr(colnames(BBLiq)[20:32], 5,7), sep = "_"),
+  paste('A', substr(colnames(BBLiq)[33:37], 3,5), sep = "_"),
+  paste('B', substr(colnames(BBLiq)[38:50], 5,7), sep = "_"),
+  paste('B', substr(colnames(BBLiq)[51:55], 3,5), sep = "_") )
 
-BBVol$Date = as.Date(BBVol$Date) #converting POSIXct to Date
-BBVol[,-1] = lapply(BBVol[,-1], as.numeric)
+BBLiq$Date = as.Date(BBLiq$Date) #converting POSIXct to Date
+BBLiq[,-1] = lapply(BBLiq[,-1], as.numeric)
 
-BBVol = BBVol[order(BBVol$Date),]
+BBLiq = BBLiq[order(BBLiq$Date),]
 
 # Creating Weekly data ----------------------------------------------------
 
-
-
+VarsExport = c("TBA", "TBF", "01Y","02Y", "05Y", "10Y", "30Y")
+VarsExport = c(paste('B', VarsExport, sep = "_" ), 
+               paste('A', VarsExport, sep = "_" ))
 
 Sun  = seq(as.Date("2006-01-01"), as.Date("2023-12-31"), by = "7 days")            #creating vector of Sundays. 2006-01-01 was Sunday.
 
-Mex_w = data.frame(Date = Sun, Values = matrix(NA, length(Sun),
-                                               length(VarsExport) -1) )                #Initiating a dataframe
-colnames(Mex_w) = VarsExport
-
-colMeans(is.na(Mex_d))                                                            #checking which variables have missing values - only MTBA and GMXN10y have missing values
-length(Mex_d$Date[is.na(Mex_d$Tgt_rate)])                                         #code to be edited to check how many and which dates each variable is missing
+Liq_w = data.frame(Date = Sun,
+                     Values = matrix(NaN, length(Sun),length(VarsExport) ) )                #Initiating a dataframe
+colnames(Liq_w)[-1] = VarsExport
 
 for(i in 1:length(Sun)){                                                          
-  Week_Data = MergedTab[MergedTab$Date <= Sun[i] & MergedTab$Date > (Sun[i]-7),
-                        VarsExport]
+  Week_Data = BBLiq[BBLiq$Date <= Sun[i] & BBLiq$Date > (Sun[i]-7),
+                        c('Date',VarsExport) ]
   
-  for (V in VarsExport[-1]){
+  for (V in VarsExport){
     a = na.omit(Week_Data[V])
-    Mex_w[i, V] = if(nrow(a) != 0) tail(a,1) else NA
+    Liq_w[i, V] = if(nrow(a) != 0) tail(a,1) else NA
   }
 } 
+
+
+# Calculating bid-ask spread ----------------------------------------------
+
 
