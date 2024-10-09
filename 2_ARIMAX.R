a707545e75d2335edf8673900e59ff4ca1145579
Sebin Nidhiri <sebinbn@gmail.com>
2024-10-09 14:22:24 -0500

    Liquidity regressions done.

--- 2_ARIMAX.R
+++ 2_ARIMAX.R
@@ -1,8 +1,8 @@
 # This code estimates ARIMAX model where F_Own_p is the X variable.It uses Mex_w
-# for weekly data.
+# for weekly data on yields and Vol_w for weekly data on volatility.
 
 
-library(forecast) #for using auto.arima
+library(forecast) #for using auto.arima and Arima
 
 # Model Selection ---------------------------------------------------------
 
@@ -32,3 +32,20 @@ Model_1mo = Arima(Mex_w$MPTBA, order = c(1,1,0), xreg = Mex_w$F_Own_p*100 )
 Model_1mo
 (1-pnorm(abs(Model_1mo$coef)/sqrt(diag(Model_1mo$var.coef))))*2                 #calculating p-value
 Model_1mo$nobs
+
+
+# Estimating ARIMAX model on volatility -----------------------------------
+
+Liq_w$F_Own_p = Mex_w$F_Own_p
+
+Liq_w[is.na(Liq_w$BA_TBA),c('Date', 'BA_TBA')] #most missing values are 2023 March onwards
+Liq_w[is.na(Liq_w$BA_10Y),c('Date', 'BA_10Y')] # all missing values between 2007 Oct - 2011 July
+
+lapply(Liq_w[,c('BA_TBA',"BA_10Y")], auto.arima, max.d = 0)
+
+summary(ur.df(na.omit(Liq_w$BA_TBA), type = "none",selectlags = "AIC")) #conclude stationarity
+summary(ur.df(na.omit(Liq_w$BA_10Y), type = "none",selectlags = "AIC")) #conclude stationarity
+
+Vol_reg = lapply(Liq_w[,c('BA_TBA',"BA_TBF","BA_01Y","BA_02Y","BA_05Y","BA_10Y", "BA_30Y")], 
+       function(x){summary(lm(x~F_Own_p, data = Liq_w))} )
+
