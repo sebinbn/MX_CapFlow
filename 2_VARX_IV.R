055e41b78717c6c4f0e1d2419e18f5ca9a2f01f6
Sebin Nidhiri <sebinbn@gmail.com>
2024-10-15 17:49:54 -0500

    Added code for regular VAR.

--- 2_VARX_IV.R
+++ 2_VARX_IV.R
@@ -3,3 +3,9 @@
 
 library(vars)
 
+
+Index = cbind(
+  Mex_w_d$Date <= as.Date("2016-12-31") & Mex_w_d$Date >= as.Date("2010-01-01"))
+sample = Mex_w_d[Index, c("MPTBA","GMXN10Y","MXN_USD")]
+
+VAR_result = VAR(sample, type = "none")
