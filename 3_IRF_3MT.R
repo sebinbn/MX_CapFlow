c803e77da910be301cca6f672a647237de2069b1
Sebin Nidhiri <sebinbn@gmail.com>
2024-10-26 18:01:47 -0500

    bar plot created. Yet to create arrows.

--- 3_IRF_3MT.R
+++ 3_IRF_3MT.R
@@ -9,7 +9,7 @@ IRFVals = matrix(NaN,17,6)
 i = 0
 for (samp_num in seq(1,49,3)){
   i = i + 1
-  Maturity = c(Maturity, colnames(IRFs[[samp_num]]$irf$TIIE)[2] )
+  Maturity[i] = colnames(IRFs[[samp_num]]$irf$TIIE)[2] 
   IRFVals[i,1] = IRFs[[samp_num]]$irf$TIIE[,2][1] #picks out first obs of 2nd column from IRFs table
   IRFVals[i,2] = IRFs[[samp_num]]$Upper$TIIE[,2][1]
   IRFVals[i,3] = IRFs[[samp_num]]$Lower$TIIE[,2][1]
@@ -18,86 +18,33 @@ for (samp_num in seq(1,49,3)){
   IRFVals[i,5] = IRFs[[samp_num+1]]$Upper$TIIE[,2][1]
   IRFVals[i,6] = IRFs[[samp_num+1]]$Lower$TIIE[,2][1]
 }
-  
 
-irfGen = function(samp_num,resp_num){
-  # Logic of steps:
-  # 1.Check if required subset of IRF data has NAs 
-  # 2.Subset required data from list and store in format needed for plotting.
-  # 3.Idenitfy name of response variable
-  # 4. Plot IRF
-  
-  if ( all(is.na(IRFs[[samp_num]])) ){
-    print('No analysis had been done owing to missing value')
-    return()}
-  
-  irfDat = data.frame(Week = 0:(nrow(IRFs[[samp_num]]$irf$TIIE) - 1),
-                      IRF = IRFs[[samp_num]]$irf$TIIE[,resp_num],
-                      Upper_CI = IRFs[[samp_num]]$Upper$TIIE[,resp_num],
-                      Lower_CI = IRFs[[samp_num]]$Lower$TIIE[,resp_num])
-  
-  
-  # Before plotting I identify a long and short name for impulse variable and
-  # response variable. long name is used to label figure and short name to save file
-  
-  # Identify response variable name from sample
-  resp_var = colnames(IRFs[[samp_num]]$Upper$TIIE)[2]
-  if (resp_var %in% c("MPTBA", "MPTBC", "MPTBF", "MPTBI") ){
-    resp = substr(resp,5,5)
-    if (resp == "A"){ mo = "1"}
-    if (resp == "C"){ mo = "3"}
-    if (resp == "F"){ mo = "6"}
-    if (resp == "I"){ mo = "9"}
-    resp = paste(mo,"mo", sep = "")
-    Resp = paste(mo,"mo", sep = " ")
-  }else{
-    resp = paste(substr(resp_var,5,6),"y", sep = "")
-    Resp = paste(substr(resp_var,5,6),"yr", sep = " ")
-  }
-  
-  implse = "ON"
-  Implse = "Overnight"
-  
-  # Identify period from sample number
-  if (samp_num %% 3 == 1){
-    period = '2010-11'
-  }else if (samp_num %% 3 == 2){
-    period = '2012-13'
-  }else if (samp_num %% 3 == 0){
-    period = '2014-15'
-  }
-  
-  IRFPlot = ggplot(irfDat, aes(x = Week)) +
-    geom_line(aes(y = IRF), color = "black", linewidth = 1.2) +                     # Main IRF line
-    geom_ribbon(aes(ymin = Lower_CI, ymax = Upper_CI), fill = 'blue', alpha = 0.3) +  # CI band
-    geom_hline(yintercept = 0, color = "red", linetype = "dashed", linewidth = 1) +  # Zero line
-    scale_y_continuous(limits = c(-0.4, 0.85), breaks = seq(-0.2, 0.8, by = 0.1)) +
-    labs(title = paste("Impulse Response to", Implse, "yield"), x = "Week",
-         y = paste('\u0394', Resp, "yield") ) +
-    theme_minimal() +
-    theme(axis.title.x = element_text(size = 14),
-          axis.title.y = element_text(size = 14),
-          axis.text = element_text(size = 12))
-  
-  #Saving plot to file
-  path = substr(getwd(), 1, nchar(getwd()) - 13)                                  # going up 1 directory
-  FileName = paste(period,implse,resp, sep = "_")
-  path = paste(path,'Paper/Image_fromCode/',FileName,".png", sep = "")
-  ggsave(filename = path, plot = IRFPlot, width = 5, height = 4, dpi = 300)
-  
-  return(IRFPlot)
-  
-  
-}
+Maturity = paste("Yr",substr(Maturity,5,6))
+Maturity[14:17] = c("Mo 01","Mo 03","Mo 09","Mo 06")
 
+irfDat = data.frame(Maturity = Maturity, Values = IRFVals)
+colnames(irfDat)[-1] = c("L_IRF","L_Up", "L_Low","H_IRF","H_Up", "H_Low") #intial L/H shows high/low FO period, IRF is value, Up and Low are upper and lower bounds respectively
+                    
 
-# Creating plots ----------------------------------------------------------
+df_long <- melt(irfDat[c("Maturity","L_IRF","H_IRF")], 
+                id.vars = 'Maturity', variable.name = 'Period')
 
-IRF_GGplot = list()
-sampleNums = c(seq(1,49,3), seq(2,50,3) )
+# Create the ggplot with two lines
+ggplot(df_long, aes(x = Maturity, y = value, fill = Period)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = 'Maturity', y = 'Transmission', title = 'Lower transmission') +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14),
+        legend.title = element_blank(), legend.position = c(0.1, 0.9))
 
-for (i in sampleNums){
-  irfpic = irfGen(i,2)
-  IRF_GGplot = c(IRF_GGplot, list(irfpic) ) 
-}
+ggplot(df_long, aes(x = Maturity, y = value, fill = Period)) +
+  geom_line(arrow = arrow(length=unit(0.30,"cm"), ends="first", type = "closed")) +
+  labs(x = 'Maturity', y = 'Transmission', title = 'Lower transmission') +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14),
+        legend.title = element_blank(), legend.position.inside = c(0.1, 0.9))
 
