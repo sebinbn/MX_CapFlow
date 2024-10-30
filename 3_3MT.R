b6a3e9fb327719a094bbde0f7179af4245325a37
Sebin Nidhiri <sebinbn@gmail.com>
2024-10-30 12:09:15 -0500

    Modified plots to be more readable.

--- 3_3MT.R
+++ 3_3MT.R
@@ -7,23 +7,27 @@ LowFO_mean = colMeans(BBYield[BBYield$Date >= as.Date("2006-06-01") &
 HighFO_mean = colMeans(BBYield[BBYield$Date >= as.Date("2013-01-01") & 
                    BBYield$Date <= as.Date("2018-12-31"),c(15,16,19,18,2:14)],
                    na.rm = T)
-name = paste("Y_",substr(names(LowFO_mean),5,6))
-name[1:4] = c("M_01","M_03","M_06","M_09")
+Maturity = paste(substr(names(LowFO_mean),5,6),"Yr")
+Maturity[1:4] = c("1 Mo","3 Mo","6 Mo","9 Mo")
 
-df_means <- data.frame(column_names = name, # column names (x-axis)
+df_means <- data.frame(Maturity = Maturity, # column names (x-axis)
                         LowFO = LowFO_mean,            # column means for period 1
                          HighFO = HighFO_mean)            # column means for period 2
+df_means$Maturity = factor(df_means$Maturity,levels = df_means$Maturity) 
 
 # Melt the data to make it long-form (required for ggplot)
-df_long <- melt(df_means, id.vars = 'column_names', variable.name = 'Period',
+df_long <- melt(df_means, id.vars = 'Maturity', variable.name = 'Period',
                 value.name = 'Mean')
 
 # Create the ggplot with two lines
-ggplot(df_long, aes(x = column_names, y = Mean, color = Period, group = Period)) +
+ggplot(df_long, aes(x = Maturity, y = Mean, color = Period, group = Period)) +
   geom_line(size = 1.2) +
-  labs(x = 'Maturity', y = 'Avg. Yield', title = 'Lower Yields with Larger foreigners') +
+  scale_color_discrete(labels = c("LowFO" = "Low FO", "HighFO" = "High FO")) +
+  labs(x = 'Maturity', y = 'Avg. Yield (in %)', title = 'Lower Yields in High Foreign ownership period') +
   theme_minimal()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-        axis.text = element_text(size = 12),
+  theme(title = element_text(size = 16),
+        axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text = element_text(size = 14),
         axis.title = element_text(size = 14),
+        legend.text = element_text(size = 14),
         legend.title = element_blank(), legend.position = c(0.1, 0.9))
--- 3_IRF_3MT.R
+++ 3_IRF_3MT.R
@@ -25,28 +25,26 @@ Maturity[15:18] = c("1 Mo","3 Mo","9 Mo","6 Mo")
 irfDat = data.frame(Maturity = Maturity, Values = IRFVals)
 colnames(irfDat)[-1] = c("L_IRF","L_Up", "L_Low","H_IRF","H_Up", "H_Low") #intial L/H shows high/low FO period, IRF is value, Up and Low are upper and lower bounds respectively
 irfDat$Maturity = factor(irfDat$Maturity, 
-                         levels = Maturity[c(1,15,16,18,17,2:14)])                    
+                         levels = Maturity[c(1,15,16,18,17,2:14)])         #converting to factor to control order on x-axis.           
 
 df_long <- melt(irfDat[c("Maturity","L_IRF","H_IRF")], 
                 id.vars = 'Maturity', variable.name = 'Period')
 
-# Create the ggplot with two lines
-ggplot(df_long, aes(x = Maturity, y = value, fill = Period)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  labs(x = 'Maturity', y = 'Transmission (in % points)', title = 'Lower transmission') +
-  scale_fill_discrete(labels = c("L_IRF" = "Low FO", "H_IRF" = "High FO"))+
-  theme_minimal()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-        axis.text = element_text(size = 14),
-        axis.title = element_text(size = 14),
-        legend.title = element_blank(), legend.position = c(0.9, 0.9))
 
 ggplot(df_long, aes(x = Maturity, y = value, fill = Period)) +
-  geom_line(arrow = arrow(length=unit(0.30,"cm"), ends="first", type = "closed")) +
-  labs(x = 'Maturity', y = 'Transmission', title = 'Lower transmission') +
+  geom_segment(aes(x = Maturity, xend = Maturity, y = 0, yend = value, color = Period),
+               size = 1.5,
+               arrow = arrow(length = unit(0.15, "inches"))) +
+  scale_color_discrete(labels = c("L_IRF" = "Low FO", "H_IRF" = "High FO")) +
+  labs(x = 'Maturity', y = 'Transmission (in % points)',
+       title = 'Lower transmission in High foreign ownership period') +
   theme_minimal()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-        axis.text = element_text(size = 12),
+  theme(title = element_text(size = 16),
+        axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text = element_text(size = 14),
         axis.title = element_text(size = 14),
-        legend.title = element_blank(), legend.position.inside = c(0.9, 0.9))
+        legend.text = element_text(size = 14),
+        legend.title = element_blank(), legend.position = c(0.9, 0.9))
+
+
 
