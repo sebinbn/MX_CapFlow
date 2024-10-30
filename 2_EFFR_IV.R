52f0a28e68542f2e83080f0a6cb48468db0b413b
Sebin Nidhiri <sebinbn@gmail.com>
2024-10-30 15:39:10 -0500

    Added code to check stationarity of EFFR before doing IV regressions.

--- 2_EFFR_IV.R
+++ 2_EFFR_IV.R
@@ -1,5 +1,5 @@
-# This file runs following IV regressions
-# 1. 2SLS regression on bid-ask spreads
+# This file uses EFFR_w, Liq_w and Mex_w to run following IV regressions
+# 1. 2SLS regression of bid-ask spreads (in bps) on foreign ownership (in percent points)
 # 2. Using 1st stage fitted values as X in ARIMAX model on yields
 
 
@@ -10,28 +10,42 @@ library(urca) # for ur.df
 # Checking Relevance ------------------------------------------------------
 
 ##Plotting EFFR and Capital inflows
-# EFFR_long = melt(EFFR_w, id.vars = "Date")
+# EFFR_long = melt(IVRegData, id.vars = "Date")
 # ggplot( data = EFFR_long, aes(x = Date, y = value, color = variable)) +
 #   geom_line(linewidth = 1.25)
 
 
 # 2SLS - IV ------------------------------------------------------------
 
-## Adding necessary columns ------------------------------------------------
+## Creating dataframe ------------------------------------------------
 
-EFFR_w$F_Own_pcnt = Mex_w$F_Own_p *100                                            # Adding columns from other datatables
-EFFR_w[c('BA1mo', 'BA10Y')] = Liq_w[c('BA_TBA', 'BA_10Y')]*100
+IVRegData$F_Own_pcnt = Mex_w$F_Own_p *100                                            # Adding columns from other datatables
+#BA10y variable has many NAs between 2007 Oct and 2011 July and BA1y has few NA 
+# values before 2023 Mar. So, analysis is done until 2022 Dec.
+IVRegData[c('BA1mo', 'BA10Y')] = na.approx(Liq_w[c('BA_TBA', 'BA_10Y')]*100) 
+IVRegData = IVRegData[IVRegData$Date < as.Date("2022-12-31"),]
 
+
+## Running ADF test --------------------------------------------------------
+
+ADFresults =  apply(IVRegData[-1], MARGIN = 2, FUN = ur.df, type = "none")
+for (i in 1:4){
+  print(paste("p-value of ADF stat at levels for",colnames(IVRegData)[i+1],"is", 
+              ADFresults[[i]]@testreg$coefficients[1,4]))
+}
+
+# from the results, I can see that there is unit root in EFFR and F_Own_pcnt. 
+# the BA spreads are both stationary.
 
 # 2SLS on BA Spread  ------------------------------------------------------
 
-Stage1 = lm(F_Own_pcnt~ EFFR, data = EFFR_w)
-Stage2 = lm(EFFR_w$BA1mo ~ Stage1$fitted.values)
+Stage1 = lm(F_Own_pcnt~ EFFR, data = IVRegData)
+Stage2 = lm(IVRegData$BA1mo ~ Stage1$fitted.values)
 summary(Stage1)
 
-IVreg_1mo = ivreg(BA1mo ~ F_Own_pcnt | EFFR, data = EFFR_w)
+IVreg_1mo = ivreg(BA1mo ~ F_Own_pcnt | EFFR, data = IVRegData)
 summary(IVreg_1mo)
-IVreg_10y = ivreg(BA10Y ~ F_Own_pcnt | EFFR, data = EFFR_w)
+IVreg_10y = ivreg(BA10Y ~ F_Own_pcnt | EFFR, data = IVRegData)
 summary(IVreg_10y)
 
 # 2SLS ARIMAX on yields ------------------------------------------------------
@@ -45,7 +59,7 @@ for(i in 1:3){
   print(BIC(test_10y))
 }                                                                                 #tested AR(i), MA(i) and conclude that AR(1,1,0) is best
 
-#testing Unit root in fitted Foreing Ownership
+#testing Unit root in fitted Foreign Ownership
 summary(ur.df(Stage1$fitted.values, type = "none"))
 summary(ur.df(diff(Stage1$fitted.values), type = "none"))                                #there is unit root
 
