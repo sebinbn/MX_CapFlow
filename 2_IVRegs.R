8b1115ee55e480ccfd6e006658f6ba44db4a4ed8
Sebin Nidhiri <sebinbn@gmail.com>
2024-10-30 18:31:44 -0500

    Modified to run all regressions on 1st diff. EFFR and F_Own_p have unit root. spreads also differenced even if they dont have unit root.

--- 2_IVRegs.R
+++ 2_IVRegs.R
@@ -1,6 +1,10 @@
 # This file uses EFFR_w, Liq_w and Mex_w to run following IV regressions
 # 1. 2SLS regression of bid-ask spreads (in bps) on foreign ownership (in percent points)
 # 2. Using 1st stage fitted values as X in ARIMAX model on yields
+# It returns the results in a list whose components are 
+# Stage1 (first stage OLS), IVReg_1mo (2nd stage on 1mo spread), 
+# IVReg_10y(2nd stage on 10y spread), Model_10y (ARIMAX with IV on 10yr yield),
+# Model_1mo (ARIMAX with IV on 10yr yield)
 
 
 library(ivreg)
@@ -18,12 +22,12 @@ library(urca) # for ur.df
 # 2SLS - IV ------------------------------------------------------------
 
 ## Creating dataframe ------------------------------------------------
-
+IVRegData = EFFR_w
 IVRegData$F_Own_pcnt = Mex_w$F_Own_p *100                                            # Adding columns from other datatables
 #BA10y variable has many NAs between 2007 Oct and 2011 July and BA1y has few NA 
 # values before 2023 Mar. So, analysis is done until 2022 Dec.
 IVRegData[c('BA1mo', 'BA10Y')] = na.approx(Liq_w[c('BA_TBA', 'BA_10Y')]*100) 
-IVRegData = IVRegData[IVRegData$Date < as.Date("2022-12-31"),]
+IVRegData = IVRegData[IVRegData$Date <= as.Date("2022-12-31"),]
 
 
 ## Running ADF test --------------------------------------------------------
@@ -37,43 +41,60 @@ for (i in 1:4){
 # from the results, I can see that there is unit root in EFFR and F_Own_pcnt. 
 # the BA spreads are both stationary.
 
+#taking first differences
+IVData_stat = IVRegData[-1,]
+#IVData_stat[c("EFFR", "F_Own_pcnt")]  = apply(IVRegData[c("EFFR", "F_Own_pcnt")], 2, diff)
+IVData_stat[-1] = apply(IVRegData[-1], 2, diff)
 # 2SLS on BA Spread  ------------------------------------------------------
 
-Stage1 = lm(F_Own_pcnt~ EFFR, data = IVRegData)
-Stage2 = lm(IVRegData$BA1mo ~ Stage1$fitted.values)
+Stage1 = lm(F_Own_pcnt~ 0 + EFFR, data = IVData_stat)
+#Stage2 = lm(IVData_stat$BA1mo ~ Stage1$fitted.values)
 summary(Stage1)
 
-IVreg_1mo = ivreg(BA1mo ~ F_Own_pcnt | EFFR, data = IVRegData)
+IVreg_1mo = ivreg(BA1mo ~ 0 + F_Own_pcnt | EFFR, data = IVData_stat)
 summary(IVreg_1mo)
-IVreg_10y = ivreg(BA10Y ~ F_Own_pcnt | EFFR, data = IVRegData)
+IVreg_10y = ivreg(BA10Y ~ 0 + F_Own_pcnt | EFFR, data = IVData_stat)
 summary(IVreg_10y)
 
 # 2SLS ARIMAX on yields ------------------------------------------------------
 
-#Checking ARIMA structure for 10yr and 1mo yield.
-lapply(Mex_w[,c('MPTBA',"GMXN10Y")], auto.arima, max.d = 1, xreg = Stage1$fitted.values) #suggest 1,1,1 for 1mo and 0,1,5 for 10yr
+#getting yield data and first differencing
+IVData_stat[c('MPTBA',"GMXN10Y")] = apply(na.approx(
+  Mex_w[Mex_w$Date <= as.Date("2022-12-31") ,c('MPTBA',"GMXN10Y")]),
+  MARGIN = 2, FUN = diff)
 
-for(i in 1:3){
-  test_10y = arima(Mex_w$GMXN10Y, order = c(0,1,i))
-  print(test_10y)
-  print(BIC(test_10y))
-}                                                                                 #tested AR(i), MA(i) and conclude that AR(1,1,0) is best
-
-#testing Unit root in fitted Foreign Ownership
-summary(ur.df(Stage1$fitted.values, type = "none"))
-summary(ur.df(diff(Stage1$fitted.values), type = "none"))                                #there is unit root
+# #Checking ARIMA structure for 10yr and 1mo yield.
+# lapply(Mex_w[,c('MPTBA',"GMXN10Y")], auto.arima, max.d = 1, xreg = Stage1$fitted.values) #suggest 1,1,1 for 1mo and 0,1,5 for 10yr
+# 
+# for(i in 1:3){
+#   test_10y = arima(Mex_w$GMXN10Y, order = c(0,1,i))
+#   print(test_10y)
+#   print(BIC(test_10y))
+# }                                                                                 #tested AR(i), MA(i) and conclude that AR(1,1,0) is best
+# 
+# #testing Unit root in fitted Foreign Ownership
+# summary(ur.df(Stage1$fitted.values, type = "none"))
+# summary(ur.df(diff(Stage1$fitted.values), type = "none"))                                #there is unit root
 
 # Estimating ARIMAX model --------------------------------------------------
 
-Model_10y = Arima(Mex_w$GMXN10Y, order = c(1,1,0), xreg = Stage1$fitted.values )
+Model_10y = Arima(IVData_stat$GMXN10Y, order = c(1,0,0), 
+                  xreg = Stage1$fitted.values, include.mean = F )
 Model_10y
 (1-pnorm(abs(Model_10y$coef)/sqrt(diag(Model_10y$var.coef))))*2                 #calculating p-value
 Model_10y$nobs
 
 # Although AIC suggested ARIMA(1,1,1) for 1mo, I use ARIMA(1,1,0) because the 
 # coefficients have similar magnitude and opposite signs indicating spurious regression
-Model_1mo = Arima(Mex_w$MPTBA, order = c(1,1,0), xreg = Stage1$fitted.values )
+Model_1mo = Arima(IVData_stat$MPTBA, order = c(1,0,0),
+                  xreg = Stage1$fitted.values, include.mean = F)
 Model_1mo
 (1-pnorm(abs(Model_1mo$coef)/sqrt(diag(Model_1mo$var.coef))))*2                 #calculating p-value
 Model_1mo$nobs
 
+
+# Removing unnecessary variables ------------------------------------------
+IVRegResults = list(Stg1 = Stage1, Stg2_1mo = IVreg_1mo, Stg2_10y = IVreg_10y,
+                    ARIMAX1mo = Model_1mo, ARIMAX10y = Model_10y)
+
+rm(IVRegData,IVData_stat, ADFresults,Stage1, IVreg_1mo,IVreg_10y, Model_1mo, Model_10y)
