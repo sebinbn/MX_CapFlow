d82acdf7c89ca0cba2630b741aa3a9c83c27b838
Sebin Nidhiri <sebinbn@gmail.com>
2024-11-14 15:36:41 -0600

    Plotting IRF and CumIRF across horizon for the 3 specifications.

--- 3_IRF_MH.R
+++ 3_IRF_MH.R
@@ -1,29 +1,128 @@
-# this code uses the list 'IRFs' and 'samp_dates' created by 2_SVAR_MH and creates a plot of contemporary
-# responses from the IRFs for various periods
+# This code uses the list 'IRFs' and 'samp_dates' created by 2_SVAR_MH and 
+# creates a plot of contemporary responses from the IRFs for various periods. 
+# Three plots are created; one for each combination of SVAR variables.
 
 
 Dates = rep(as.Date("2000-01-01"),length(samp_dates)) 
-IRFVals = matrix(NaN,length(samp_dates),3) #3 columns for IRF value, upper and lower bound
+IRF_MH_Data = list() # a list to store the data for three pictures
+CumIRF_MH_Data = list() # a list to store the data for three pictures
 
-for (i in 1:length(samp_dates)){
-  Dates[i] = samp_dates[[i]][1]
-  # IRFVals[i,1] = IRFs[[i]]$irf$MPTBA[,2][1] #picks out first obs of 2nd column from IRFs table
-  # IRFVals[i,2] = IRFs[[i]]$Upper$MPTBA[,2][1]
-  # IRFVals[i,3] = IRFs[[i]]$Lower$MPTBA[,2][1]
+for (Spec in 1:3){
   
-  IRFVals[i,1] = IRFs[[i]]$irf$TIIE[,2][1] #picks out first obs of 2nd column from IRFs table
-  IRFVals[i,2] = IRFs[[i]]$Upper$TIIE[,2][1]
-  IRFVals[i,3] = IRFs[[i]]$Lower$TIIE[,2][1]
-}
+  IRFVals = matrix(NaN,length(samp_dates),3) #3 columns for IRF value, upper and lower bound
+  CumIRFVals = matrix(NaN,length(samp_dates),1) #1 column for Cum IRF value
+  for (i in 1:length(samp_dates)){
+    if(Spec ==1){Dates[i] = samp_dates[[i]][1]}
+    
+    if (Spec == 3){
+      IRFVals[i,1] = IRFs[[Spec]][[i]]$irf$MPTBA[,2][1] #picks out first obs of 2nd column from IRFs table
+      IRFVals[i,2] = IRFs[[Spec]][[i]]$Upper$MPTBA[,2][1]
+      IRFVals[i,3] = IRFs[[Spec]][[i]]$Lower$MPTBA[,2][1]
+      
+      CumIRFVals[i,1] = sum(IRFs[[Spec]][[i]]$irf$MPTBA[,2]) #sum of 2nd column from IRFs table
+    }else{
+      IRFVals[i,1] = IRFs[[Spec]][[i]]$irf$TIIE[,2][1] #picks out first obs of 2nd column from IRFs table
+      IRFVals[i,2] = IRFs[[Spec]][[i]]$Upper$TIIE[,2][1]
+      IRFVals[i,3] = IRFs[[Spec]][[i]]$Lower$TIIE[,2][1]
+      
+      CumIRFVals[i,1] = sum(IRFs[[Spec]][[i]]$irf$TIIE[,2]) #sum of 2nd column from IRFs table
+    }
+    
+  }
+  
+  irfDat = data.frame(Dates = Dates, Values = IRFVals)
+  colnames(irfDat)[-1] = c("IRF","Up", "Low") 
+  IRF_MH_Data = c(IRF_MH_Data, list(irfDat))
+  
+  CumIRFDat = data.frame(Dates = Dates, Values = CumIRFVals)
+  colnames(CumIRFDat)[-1] = "CumIRF" 
+  CumIRF_MH_Data = c(CumIRF_MH_Data, list(CumIRFDat))
+ 
+} #end of Spec loop 
 
-irfDat = data.frame(Dates = Dates, Values = IRFVals)
-colnames(irfDat)[-1] = c("IRF","Up", "Low") 
 
+# Creating Plots ----------------------------------------------------------
 
-ggplot(irfDat, aes(x = Dates, y = IRF)) +
+## Plotting ON_1mo ---------------------------------------------------------
+
+  ggplot(IRF_MH_Data[[1]], aes(x = Dates, y = IRF)) +
+    geom_line(linewidth = 1.2) +
+    scale_x_date(date_breaks = "2 year", date_labels = "%Y")+
+    labs(x = element_blank(), y = 'Contemporaneous Transmission (in % points)',
+         title = 'Impulse = Overnight rate, Response = 1 mo yield') +
+    theme_minimal()+
+    theme(title = element_text(size = 14),
+          axis.text.x = element_text(angle = 45, hjust = 1), 
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          legend.text = element_text(size = 14),
+          legend.title = element_blank(), legend.position = c(0.9, 0.9))
+
+## Plotting ON_10y ---------------------------------------------------------
+ggplot(IRF_MH_Data[[2]], aes(x = Dates, y = IRF)) +
   geom_line(linewidth = 1.2) +
-  labs(x = 'Date', y = 'Contemporaneous Transmission (in % points)',
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y")+
+  labs(x = element_blank(), y = 'Contemporaneous Transmission (in % points)',
+       title = 'Impulse = Overnight rate, Response = 10 yr yield') +
+  theme_minimal()+
+  theme(title = element_text(size = 14),
+        axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_blank(), legend.position = c(0.9, 0.9))
+
+## Plotting 1mo_10y ---------------------------------------------------------
+ggplot(IRF_MH_Data[[3]], aes(x = Dates, y = IRF)) +
+  geom_line(linewidth = 1.2) +
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y")+
+  labs(x = element_blank(), y = 'Contemporaneous Transmission (in % points)',
+       title = 'Impulse = 1 mo yield, Response = 10 yr yield') +
+  theme_minimal()+
+  theme(title = element_text(size = 14),
+        axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_blank(), legend.position = c(0.9, 0.9))
+
+# Creating Cumulative Plots ----------------------------------------------------------
+
+## Plotting ON_1mo ---------------------------------------------------------
+
+ggplot(CumIRF_MH_Data[[1]], aes(x = Dates, y = CumIRF)) +
+  geom_line(linewidth = 1.2) +
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y")+
+  labs(x = element_blank(),y = 'Cumulative Transmission (in % points)',
        title = 'Impulse = Overnight rate, Response = 1 mo yield') +
+  theme_minimal()+
+  theme(title = element_text(size = 14),
+        axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_blank(), legend.position = c(0.9, 0.9))
+
+## Plotting ON_10y ---------------------------------------------------------
+ggplot(CumIRF_MH_Data[[2]], aes(x = Dates, y = CumIRF)) +
+  geom_line(linewidth = 1.2) +
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y")+
+  labs(x = element_blank(), y = 'Cumulative Transmission (in % points)',
+       title = 'Impulse = Overnight rate, Response = 10 yr yield') +
+  theme_minimal()+
+  theme(title = element_text(size = 14),
+        axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_blank(), legend.position = c(0.9, 0.9))
+
+## Plotting 1mo_10y ---------------------------------------------------------
+ggplot(CumIRF_MH_Data[[3]], aes(x = Dates, y = CumIRF)) +
+  geom_line(linewidth = 1.2) +
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y")+
+  labs(x = element_blank(), y = 'Cumulative Transmission (in % points)',
+       title = 'Impulse = 1 mo yield, Response = 10 yr yield') +
   theme_minimal()+
   theme(title = element_text(size = 14),
         axis.text.x = element_text(angle = 45, hjust = 1), 
