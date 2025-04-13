6754931afb51d186a0839352c891121c0302605f
unknown <sebinbn@gmail.com>
2025-04-13 16:15:19 -0500

    Removed double error bars. updated labels of plot to show actual value and not proportion.

--- 3_IV_Mat.R
+++ 3_IV_Mat.R
@@ -9,10 +9,11 @@ Y_FO_Dat$Var = factor(Y_FO_Dat$Var,levels = Y_FO_Dat$Var)
 
 ggplot(Y_FO_Dat, aes(x =Var)) +
   geom_point(aes(y = coef), size = 2) +
-  geom_errorbar(aes(ymin = coef - se, ymax = coef + se), width = 0.2, color = "blue") +  # Error bars
+  geom_errorbar(aes(ymin = coef - tFse, ymax = coef + tFse), width = 0.2, color = "blue") +  # tF Error bars
+  #geom_errorbar(aes(ymin = coef - se, ymax = coef + se), width = 0.2, color = "blue") +  # Error bars
   geom_hline(yintercept = 0, linetype = "dashed", color = "red", size = 1) + 
   scale_x_discrete(breaks = Y_FO_Dat$Var[seq(1, length(Y_FO_Dat$Var), by = 2)])+
-  labs(x = 'Maturity', y = 'Basis points', title = 'Impact on yields from 1pp increase in FO') +
+  labs(x = 'Maturity', y = 'Basis points', title = 'Impact on yields from a 1bn MX$ increase in F-OwnBond') +
   theme_minimal()+
   theme(title = element_text(size = 16),
         axis.text.x = element_text(angle = 45, hjust = 1), 
@@ -32,10 +33,11 @@ S_FO_Dat$Var = factor(S_FO_Dat$Var,levels = S_FO_Dat$Var)
 
 ggplot(S_FO_Dat, aes(x =Var)) +
   geom_point(aes(y = coef), size = 2) +
-  geom_errorbar(aes(ymin = coef - se, ymax = coef + se), width = 0.2, color = "blue") +  # Error bars
+  geom_errorbar(aes(ymin = coef - tFse, ymax = coef + tFse), width = 0.2, color = "blue") +  # tF Error bars
+  #geom_errorbar(aes(ymin = coef - se, ymax = coef + se), width = 0.2, color = "blue") +  # Error bars
   geom_hline(yintercept = 0, linetype = "dashed", color = "red", size = 1) + 
   scale_x_discrete(breaks = Y_FO_Dat$Var[seq(1, length(Y_FO_Dat$Var), by = 2)])+
-  labs(x = 'Maturity', y = 'Basis points', title = 'Impact on spreads from 1pp increase in FO') +
+  labs(x = 'Maturity', y = 'Basis points', title = 'Impact on spreads from a 1bn MX$ increase in F-OwnBond') +
   theme_minimal()+
   theme(title = element_text(size = 16),
         axis.text.x = element_text(angle = 45, hjust = 1), 
