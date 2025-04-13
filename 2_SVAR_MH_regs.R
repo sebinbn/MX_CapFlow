fcd7027bfae23d60f6e47e45e7946e017b76a2c1
unknown <sebinbn@gmail.com>
2025-04-13 16:13:41 -0500

    Modified to run regression on actual value and not proportions. Doing regression with ARIMA errors instead of lm regression.

--- 2_SVAR_MH_regs.R
+++ 2_SVAR_MH_regs.R
@@ -1,7 +1,7 @@
 # The results of rolling horizon SVAR are regressed on foreign ownership. Uses
 # MergedDat and #MergedCumDat created by 3_IRF_MH.R
 
-#The units of Mex_Fo is share *10, this is adjusted in 3_IRF_MH.R
+#The units of Mex_Fo is the actual value.
 
 # Regressions on contemporaneous IRF --------------------------------------
 
@@ -25,10 +25,37 @@ summary(regON1moCum)
 regON30yCum = lm(IRF_ON30y~FO, data = MergedCumDat)
 summary(regON30yCum)
 
+AR1mo30yCum = Arima(MergedCumDat$IRF_1mo30y, order = c(1,0,0), xreg = cbind(MergedCumDat$FO))
+(1-pnorm(abs(AR1mo30yCum$coef)/sqrt(diag(AR1mo30yCum$var.coef))))*2
+ARON1moCum = Arima(MergedCumDat$IRF_ON1mo, order = c(1,0,0), xreg = cbind(MergedCumDat$FO))
+(1-pnorm(abs(ARON1moCum$coef)/sqrt(diag(ARON1moCum$var.coef))))*2
+
+# Cochrane-Orcutt Procedure ----------------------------------------
+plot(reg1mo30yCum$residuals)
+pacf(reg1mo30yCum$residuals) # shows significant 1st lag
+CO_AR_1mo30y = arima(reg1mo30yCum$residuals, order = c(1,0,0), include.mean = F)
+CO_AR_1mo30y$coef
+
+plot(regON1moCum$residuals)
+pacf(regON1moCum$residuals) # also shows significant 1st lag
+CO_AR_ON1mo = arima(regON1moCum$residuals, order = c(1,0,0), include.mean = F)
+CO_Proc_AR_ON1mo$coef
+
+
+# Other (previous code) ----------------------------------------
 # reg1mo10yCum = lm(IRF_1mo10y~FO, data = MergedCumDat)
 # summary(reg1mo10yCum)
 # 
 # regON1moCum1 = lm(IRF_ON1mo~FO, data = MergedCumDat)
 # summary(regON1moCum1)
 
+# some summary stats to put the outcome in perspective
 
+colMeans(Mex_FO[Mex_FO$Date <= as.Date("2019-02-25") & Mex_FO$Date >= as.Date("2018-02-25"),
+           "F_Own"])
+colMeans(Mex_FO[Mex_FO$Date <= as.Date("2023-12-25") & Mex_FO$Date >= as.Date("2023-01-25"),
+                "F_Own"])
+colMeans(Mex_FO[Mex_FO$Date <= as.Date("2010-12-25") & Mex_FO$Date >= as.Date("2010-01-25"),
+                "F_Own"])
+colMeans(Mex_FO[Mex_FO$Date <= as.Date("2014-12-25") & Mex_FO$Date >= as.Date("2014-01-25"),
+                "F_Own"])
